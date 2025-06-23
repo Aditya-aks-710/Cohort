053509096853c92a6e3f61f1c269a7da9398ace7
AdiTya <adityaaryan3012@gmail.com>
2025-06-23 19:48:03 +0530

    Browser icon with increasing count

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import "./App.css"
 import logo from "./assets/react.svg"
 import globe from "./assets/worldwide.png"
 
 
 function App() {
-  const [posts, setPosts] = useState([]);
+  const [count, setCount] = useState(0);
 
-  const postComponents = posts.map(post => <Post
-    name={post.name}
-    subtitle={post.subtitle}
-    time={post.time}
-    image={post.image}
-    description={post.description}
-  />)
-
-  function addPost() {
-    setPosts([...posts, {
-      name: "Aditya",
-      subtitle: "20K followers",
-      time: "2m ago",
-      image: globe,
-      description: "Want to know how i fucked my entire internship chances at texas. Check out the video. Thank YOU"
-    }])
+  function increaseCount() {
+    setCount(prev => prev + 1)
   }
 
+  useEffect(function() {
+    const timer = setInterval(increaseCount, 1000);
+    return function(){
+      clearInterval(timer);
+    }
+  }, []);
 
   return (
-    <div id='top-container'>
-      <div id='container'>
-        {postComponents}
+    <div>
+      <div className='icon'>
+        <img src={globe} alt="" />
       </div>
-      <button onClick={addPost}>Click here</button>
+      <div className='timer'>
+        <div className='number'>
+          {count}
+        </div>
+      </div>
     </div>
   )
 }
 
-function Post({name, subtitle, time, image, description}) {
-  return (
-    <div id='post-container'>
-      <div id='post-header'>
-        <div id='profile-pic'>
-          <img src={logo}/>
-        </div>
-        <div id='profile-data'>
-          <b>{name}</b>
-          <div>{subtitle}</div>
-          {time && <div id='time'>
-            {time}
-            <img src={image}/>
-          </div>}
-        </div>
-      </div>
-      <div id='post-description'>
-        <div>{description}</div>
-      </div>
-    </div>
-  )
-}
 
 export default App
