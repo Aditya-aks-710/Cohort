43549dc89a3c9e212418a25cedab29e7a7470118
AdiTya <adityaaryan3012@gmail.com>
2025-06-24 14:55:43 +0530

    Todo Tabs with fetch

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,49 @@ import globe from "./assets/worldwide.png"
 
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function increaseCount() {
-    setCount(prev => prev + 1)
-  }
+  const [currentTab, setcurrentTab] = useState(1);
+  const [tabData, setTabData] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(function() {
-    const timer = setInterval(increaseCount, 1000);
-    return function(){
-      clearInterval(timer);
-    }
-  }, []);
+    setLoading(true);
+    fetch("https://jsonplaceholder.typicode.com/todos/" + currentTab)
+      .then(async res => {
+        const json = await res.json();
+        setTabData(json);
+        setLoading(false);
+      })
+  }, [currentTab]);
+
+  function change(menu) {
+    setcurrentTab(menu);
+  }
 
   return (
-    <div>
-      <div className='icon'>
-        <img src={globe} alt="" />
+    <>
+      <div className='navbar'>
+        <button onClick={() => change(1)} style={{color: currentTab == 1 ? "red" : "black"}}>
+          Todo #1
+        </button>
+        <button onClick={() => change(2)} style={{color: currentTab == 2 ? "red" : "black"}}>
+          Todo #2
+        </button>
+        <button onClick={() => change(3)} style={{color: currentTab == 3 ? "red" : "black"}}>
+          Todo #3
+        </button>
+        <button onClick={() => change(4)} style={{color: currentTab == 4 ? "red" : "black"}}>
+          Todo #4
+        </button>
       </div>
-      <div className='timer'>
-        <div className='number'>
-          {count}
-        </div>
+      <div className='todo'>
+        {loading ? "Loading" : (
+          <>
+            <h3>{tabData.title}</h3>
+            <p>Status : {tabData.completed ? "True" : "False"}</p>
+          </>
+        )}
       </div>
-    </div>
+    </>
   )
 }
 
